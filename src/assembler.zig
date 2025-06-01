7d4387826260d92155261ad82b43ba25c2a5544c
very-blank <aapeli.saarelainen.76@gmail.com>
2025-06-01 19:12:00 +0300

    changes

--- src/assembler.zig
+++ src/assembler.zig
@@ -1,6 +1,28 @@
+const std = @import("std");
 const FirstPass = @import("HackAsm").FirstPass;
 const SecondPass = @import("HackAsm").SecondPass;
 const instruction = @import("HackAsm");
-const SymbolTable = @import("HackAsm");
+const SymbolTable = @import("HackAsm").SymbolTable;
 
-pub const Assembler = struct {};
+pub const Assembler = struct {
+    firstPass: FirstPass,
+    allocator: std.mem.Allocator,
+
+    pub fn init(allocator: std.mem.Allocator) !Assembler {
+        return .{
+            .firstPass = try FirstPass.init(allocator),
+        };
+    }
+
+    pub fn deinit(self: *Assembler) void {
+        self.firstPass.deinit();
+    }
+
+    pub fn assemble(self: *Assembler, buffer: []u8) []u8 {
+        var secondPass: SecondPass = try self.firstPass.firstPass(buffer);
+        defer secondPass.deinit();
+
+        const assembledCode = try secondPass.secondPass();
+        return assembledCode;
+    }
+};
--- src/hackAsm.zig
+++ src/hackAsm.zig
@@ -1,4 +1,4 @@
 pub const FirstPass = @import("firstPass.zig").FirstPass;
 pub const SecondPass = @import("secondPass.zig").SecondPass;
+pub const SymbolTable = @import("symbolTable.zig").SymbolTable;
 pub const instruction = @import("instruction.zig");
-pub const SymbolTable = @import("symbolTable.zig");
