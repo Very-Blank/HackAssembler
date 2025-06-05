6bb78f54bc46fcbd3a96eb598c3179470c60372b
very-blank <aapeli.saarelainen.76@gmail.com>
2025-06-05 13:56:39 +0300

    feat: Cli done.

--- src/main.zig
+++ src/main.zig
@@ -21,14 +21,19 @@ pub fn main() !void {
         std.debug.print("Debug allocator: {any}\n", .{debug_allocator.deinit()});
     };
 
+    const args = try std.process.argsAlloc(allocator);
+    defer std.process.argsFree(allocator, args);
+
     const logger: Logger = Logger.init();
-    const firstPass = try FirstPass.init(allocator);
+
+    var firstPass = try FirstPass.init(allocator);
     defer firstPass.deinit();
 
-    for (std.os.argv) |arg| {
-        const fileName = getFileName(arg);
+    for (1..args.len) |i| {
+        const fileName = try getFileName(args[i], allocator);
+        defer allocator.free(fileName);
 
-        const file: std.fs.File = try std.fs.cwd().openFile(arg, .{});
+        const file: std.fs.File = try std.fs.cwd().openFile(args[i], .{});
         defer file.close();
 
         const length: u64 = try file.getEndPos();
@@ -38,26 +43,35 @@ pub fn main() !void {
 
         _ = try file.readAll(buffer);
 
-        var secondPass: SecondPass = firstPass.firstPass(buffer, &logger);
-        secondPass.deinit();
+        var secondPass: SecondPass = try firstPass.firstPass(buffer, &logger);
+        defer secondPass.deinit();
+
+        if (secondPass.instructions.len == 0) {
+            return logger.printError("No instructions found in the file\n", .{});
+        }
 
         const hack = try secondPass.secondPass();
         defer allocator.free(hack);
 
-        const newFile: std.fs.File = try std.fs.cwd().createFile(fileName ++ ".hack", .{});
-        newFile.close();
+        const newFile: std.fs.File = try std.fs.cwd().createFile(fileName, .{});
+        defer newFile.close();
 
         try newFile.writeAll(hack);
     }
 }
 
-inline fn getFileName(buffer: []u8) ![]u8 {
-    std.debug.asser(buffer.len > 0);
+inline fn getFileName(buffer: []u8, allocator: std.mem.Allocator) ![]u8 {
+    std.debug.assert(buffer.len > 0);
     var i: u64 = buffer.len - 1;
 
     while (0 < i) : (i -= 1) {
         if (buffer[i] == '.') {
-            if (std.mem.eql(u8, buffer[i..buffer.len], ".asm")) return buffer[0..1];
+            if (std.mem.eql(u8, buffer[i..buffer.len], ".asm")) {
+                const fileName = try allocator.alloc(u8, buffer[0..i].len + 5);
+                @memcpy(fileName[0..i], buffer[0..i]);
+                @memcpy(fileName[i..fileName.len], ".hack");
+                return fileName;
+            }
         }
     }
 
